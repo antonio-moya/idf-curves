385729ca2f234ec956593de803193fe151b78457
Antonio Moya Díez <antonio.moya.diez@gmail.com>
2024-08-28 11:27:24 +0200

    Add files via upload

---
+++ IDF.java
@@ -0,0 +1,5 @@
+class IDF {
+    public static void main(String[] args) {
+        System.out.println("Calculation of Internsity-Duration-Frequency curves.");
+    }
+}
---
+++ run.bat
@@ -0,0 +1,2 @@
+javac --release 8 .\IDF.java
+java IDF
