bc80c3fdd6d6e89b226a01484daae00d11c2767f
anmodiez <antonio.moya@idrica.com>
2024-08-28 11:40:46 +0200

    commit 2

--- IDF.java
+++ IDF.java
@@ -1,5 +1,6 @@
 class IDF {
     public static void main(String[] args) {
         System.out.println("Calculation of Internsity-Duration-Frequency curves.");
+		System.out.println("commit 2.");
     }
 }
